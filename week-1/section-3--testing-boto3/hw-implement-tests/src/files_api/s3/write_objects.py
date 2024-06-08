ba5fb8278f893a09bec3b4ed19a75ec59aebce04
Amit Vikram Raj <avr13405@gmail.com>
2024-06-08 20:24:52 +0530

    added docstring to `upload_s3_object` function

--- week-1/section-3--testing-boto3/hw-implement-tests/src/files_api/s3/write_objects.py
+++ week-1/section-3--testing-boto3/hw-implement-tests/src/files_api/s3/write_objects.py
@@ -15,4 +15,13 @@ def upload_s3_object(
     content_type: Optional[str] = None,
     s3_client: Optional["S3Client"] = None,
 ) -> None:
+    """
+    Uploads a file to an S3 bucket.
+    
+    :param bucket_name: The name of the S3 bucket to upload the file to.
+    :param object_key: path to the object in the bucket.
+    :param file_content: The content of the file to upload.
+    :param content_type: The MIME type of the file, e.g. "text/plain" for a text file.
+    :param s3_client: An optional boto3 S3 client object. If not provided, one will be created.
+    """
     return
