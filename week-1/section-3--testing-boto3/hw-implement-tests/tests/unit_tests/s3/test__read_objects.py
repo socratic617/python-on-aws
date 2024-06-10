43f5c67f75ded31be3c91d05a846f8e702b4c0ce
Amit Vikram Raj <avr13405@gmail.com>
2024-06-10 10:20:56 +0530

    mirroring tests as shown in lectures using mocked_aws pytest fixture

--- week-1/section-3--testing-boto3/hw-implement-tests/tests/unit_tests/s3/test__read_objects.py
+++ week-1/section-3--testing-boto3/hw-implement-tests/tests/unit_tests/s3/test__read_objects.py
@@ -5,21 +5,20 @@ from files_api.s3.read_objects import (
     fetch_s3_objects_using_page_token,
     object_exists_in_s3,
 )
-from mypy_boto3_s3 import S3Client
 from tests.consts import TEST_BUCKET_NAME
 
 
-def test_object_exists_in_s3(s3_client: S3Client):
+def test_object_exists_in_s3(mocked_aws: None):
     ...
 
 
-def test_pagination(s3_client: S3Client):
+def test_pagination(mocked_aws: None):
     ...
 
 
-def test_mixed_page_sizes(s3_client: S3Client):
+def test_mixed_page_sizes(mocked_aws: None):
     ...
 
 
-def test_directory_queries(s3_client: S3Client):
+def test_directory_queries(mocked_aws: None):
     ...
--- week-1/section-3--testing-boto3/hw-implement-tests/tests/unit_tests/s3/test__write_objects.py
+++ week-1/section-3--testing-boto3/hw-implement-tests/tests/unit_tests/s3/test__write_objects.py
@@ -1,9 +1,8 @@
 """Test cases for `s3.write_objects`."""
 
 from files_api.s3.write_objects import upload_s3_object
-from mypy_boto3_s3 import S3Client
 from tests.consts import TEST_BUCKET_NAME
 
 
-def test_upload_s3_object(s3_client: S3Client):
+def test_upload_s3_object(mocked_aws: None):
     ...
