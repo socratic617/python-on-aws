b649660ab5c80cd48658117af0b6a4c11d2a75d5
phitoduck <eric.riddoch@gmail.com>
2024-08-14 01:40:13 -0600

    doc: wrote much of an xray notebook

--- section-4--observability/start-compose.sh
+++ section-4--observability/start-compose.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Note: this script works well with 
+#   `aws sso login --profile cloud-course`
+#   or
+#   `aws configure sso --profile cloud-course`
+
 aws configure export-credentials --profile cloud-course --format env > .env
 
 docker compose up --build
--- section-4--observability/traces/app/main.py
+++ section-4--observability/traces/app/main.py
@@ -1,3 +1,89 @@
+"""
+Why collect monitoring data? Because by the time you need it, it's too late to get it.
+
+
+Metrics - something went wrong
+Logs - how it happened (root cause)
+Traces - birds-eye view: where did it happen, what parts of the system are involved/affected, also profiling info
+
+profiling (latency), error counts/rates,  
+
+0. have an overview - show how the data types fit together and what we will build
+  - "single pane of glass", massively distributed systems, etc.
+  - talk about metrics, logs, and traces in advance
+  - show the dashboard, alerts, and other UI's that we're going to set up
+    so they have a mental framework as we go deeper
+  - describe the workflow
+    1. an alarm tells you something is wrong
+    2. you check a dashboard and consoles
+    3. you use logs to identify root cause
+    4. you use traces to 
+
+1. logs
+
+  Intro: print statements don't scale. We need to think about how MULTIPLE
+  instances of our application will be generating logs. And we need to make them
+  searchable, especially days/weeks after they were created.
+
+  - log levels (in our appliation, where are the different levels appropriate)
+  - control log level with environment variable
+  - organize logger in code, e.g. have a log_config.py file that inits the logger
+    and then show how to import and use it in all of the other files
+
+    - put log statements in 
+        - middlewares and api route handler
+        - in the routes themselves
+        - in helper functions that the routes call (potentially, or warn against this)
+
+  - how to get logs to cloudwatch
+  - cloudwatch logs concepts: log groups, log streams, log events
+    - how billing works for these
+    - how they are meant to be used
+
+  - CORRELATION: how do you find relevant logs created from
+    - API GW
+    - lambda
+    - "single pane of glass"
+    
+  - workflow for using logs (quote Alex DeBrie "what do you do with your logs" "we store them")
+    - query logs with logs insights QL (talk about cost, e.g. scanning)
+    - live tail (there is some free live tail time on the free tier)
+    - uses
+        - find errors--logs are where you find root cause, e.g. traceback
+        - find performance issues (latency)
+        - metrics
+
+  - structured logs
+
+2. metrics / alarms / dashboards
+  - 
+
+3. traces
+  - correlating logs across services
+  - understanding which services a transaction goes through (a subset, or in which order)
+  - profiling, where are latency bottlenecks in the distributed system
+  - 
+
+4. dashboards
+  - super dashboard
+    - node graph
+    - convenience URLs (in the markdown)
+      - xray and the relevant traces
+      - cloudwatch logs insights
+      - relevant cloudwatch log groups and streams
+      - resources
+        - s3
+        - api gateway
+        - lambda
+  - parameterized dashboard
+
+5. alerts
+
+6. workflow for monitoring to use all of the above
+
+
+"""
+
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
@@ -9,12 +95,12 @@ from aws_xray_sdk.core import (
     patch_all,
     xray_recorder,
 )
+from aws_xray_sdk.core.models.segment import Segment
 from fastapi import (
     FastAPI,
     HTTPException,
     Request,
 )
-from aws_xray_sdk.core.models.segment import Segment
 
 # Patch all libraries to be instrumented by AWS X-Ray
 patch_all()
@@ -30,7 +116,9 @@ xray_recorder.configure(
     # },
 )
 
+
 app = FastAPI(docs_url="/")
+
 
 @app.middleware("http")
 async def record_xray_segment(request: Request, call_next):
